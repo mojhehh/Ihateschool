7a02b260e3e9651e7ae380b15a256684764e389e
Mojheh <sebmojheh@gmail.com>
2025-10-27 16:04:44 -0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,20 @@
+// firebase-config.js
+// Put this in the repo root next to index.html and admin.html
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaNPIKQU-LVWctrZffkIJAP1-odXOdof8",
+  authDomain: "school-thingy-45207.firebaseapp.com",
+  databaseURL: "https://school-thingy-45207-default-rtdb.firebaseio.com",
+  projectId: "school-thingy-45207",
+  storageBucket: "school-thingy-45207.firebasestorage.app",
+  messagingSenderId: "1048084200731",
+  appId: "1:1048084200731:web:b0d9927aee9f4d3dbce7be",
+  measurementId: "G-7Y7KRR2EYK"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const auth = getAuth(app);
